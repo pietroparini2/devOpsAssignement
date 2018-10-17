8cc30425d5980201a425e24f560682287f4636fb
pietro <p.parini2@campus.unimib.it>
2018-10-17 23:04:34 +0200

    prova

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/product.model');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
-    res.send('Greetings from the Test controller, with jenkiens v3 !');
+    res.send('Greetings from the Test controller, with jenkiens v4 !');
 };
 
 exports.product_create = function (req, res) {
