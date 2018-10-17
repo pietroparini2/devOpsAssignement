1a8646a95c493e6ba58335b8fd4f428217e5007a
pietro <p.parini2@campus.unimib.it>
2018-10-17 23:51:32 +0200

    prova

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/product.model');
 
 //Simple version, without validation or sanitation
 exports.test = function (req, res) {
-    res.send('Greetings from the Test controller, with jenkiens dp !');
+    res.send('Greetings from the Test controller, with jenkiens dip !');
 };
 
 exports.product_create = function (req, res) {
